1a32360cc809925d1122eb844eb6824ccc17f6e9
Aaron Baker <baker.barisax@gmail.com>
2020-05-06 13:04:53 -0500

    Added logic and ability to add points
    Added logic to add Written Responses, Short Answer/Fill in the Blank, Matching, and Ordering.
    
    Also added the ability to add custom points instead of having everything set to the default amount of points

--- js/csv.js
+++ js/csv.js
@@ -76,23 +76,34 @@ function EvaluateBlock(lineBlock, lineIndex) {
 	}
 
 	if (lineBlock.length === 1) {
-		//Error, no answer options
-		errorHandler.addError("Error on line " + (lineIndex + 1) + ": There are no answer options provided for question: \"" + lineBlock[0] + "\"");
+		//Written Response using default points
+		return newWrittenResponse(lineBlock);
 	} else if (lineBlock.length === 2) {
-		if (lineBlock[1].toLowerCase() !== "true" && lineBlock[1].toLowerCase() !== "false") {
+		if (lineBlock[1].toLowerCase() !== "true" && lineBlock[1].toLowerCase() !== "false" && lineBlock[1].toLowerCase().search("points") == -1){
 			errorHandler.addError("Error on line " + (lineIndex + 1) + ": Only true or false questions can have only one answer choice, and it must be either true or false. Make sure <strong>not</strong> to include a '*' before true or false.");
+		} else if(lineBlock[1].toLowerCase().search("points") != -1) {
+			//Written Response with points
+			return newWrittenResponse(lineBlock);
+		
 		} else {
 			//Since the length of the array is only 2, this must be a true or false question.
 			return newTrueFalse(lineBlock);
 		}
-
+	} else if (lineBlock.length === 3 && lineBlock[1].toLowerCase().search("points") !== -1) {
+		//True/False with custom points
+		return newTrueFalse(lineBlock);
 	} else {
 		//Either a multiple choice or a multi-select, let's test which it is.
 		var correctAnswers = 0;
+		var match = 0;
 		for (var questionLineIndex = 0; questionLineIndex < lineBlock.length; questionLineIndex++) {
 			var subStringSample = lineBlock[questionLineIndex].substr(0, 1);
 			if (subStringSample == "*") {
 				correctAnswers++;
+			}
+			var searchMatch = lineBlock[questionLineIndex].search("/");
+			if (searchMatch != -1) {
+				match++;
 			}
 		}
 		if (correctAnswers == 1) {
@@ -101,13 +112,28 @@ function EvaluateBlock(lineBlock, lineIndex) {
 		} else if (correctAnswers > 1) {
 			//Multi-select
 			return newMultiSelect(lineBlock);
+		} else if(correctAnswers == 0) {
+			//Either Short Answer/Blank, Matching, or Ordering
+			var q = lineBlock[0].toLowerCase()
+			if(match > 1){
+				//Matching
+				return newMatching(lineBlock);
+			}
+			else if(q.search("order") != -1 || q.search("greatest") != -1 || q.search("sort") != -1)  {
+				//Order Question
+				return newOrder(lineBlock);
+			} else {
+				//Short Answer/Fill in the Blank
+				return newShortAnswer(lineBlock);
+			}
 		} else {
 			//Error, no correct answers.
 			errorHandler.addError("Error on line " + (lineIndex + 1) + ": There is no correct answer selected for question: \"" + lineBlock[0] + "\"");
-		}
-
+		  }
 	}
+
 }
+
 
 
 function newMultiSelect(lineBlock) {
@@ -116,11 +142,20 @@ function newMultiSelect(lineBlock) {
 	multiSelect.NewQuestion = 'MS';
 	multiSelect.Title = '';
 	multiSelect.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
-	multiSelect.Points = config.questionScore;
+	var startIndex = 1;
+	if(lineBlock[1].toLowerCase().search("points") != -1) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		multiSelect.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		multiSelect.Points = config.questionScore;
+	}
 	multiSelect.Difficulty = config.difficultyScore;
 	multiSelect.Options = [];
 	/* Starts at one because the first line of lineBlock is the question*/
-	for (var optionIndex = 1; optionIndex < lineBlock.length; optionIndex++) {
+	for (var optionIndex = startIndex; optionIndex < lineBlock.length; optionIndex++) {
 		var score = 0;
 		if (lineBlock[optionIndex].substr(0, 1) == "*") {
 			score = 1;
@@ -155,11 +190,20 @@ function newMultipleChoice(lineBlock) {
 	multipleChoice.NewQuestion = 'MC';
 	multipleChoice.Title = '';
 	multipleChoice.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
-	multipleChoice.Points = config.questionScore;
+	var startIndex = 1;
+	if(lineBlock[1].toLowerCase().search("points") != -1) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		multipleChoice.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		multipleChoice.Points = config.questionScore;
+	}
 	multipleChoice.Difficulty = config.difficultyScore;
 	multipleChoice.Options = [];
 	/* Starts at one because the first line of lineBlock is the question*/
-	for (var optionIndex = 1; optionIndex < lineBlock.length; optionIndex++) {
+	for (var optionIndex = startIndex; optionIndex < lineBlock.length; optionIndex++) {
 		var score = 0;
 		if (lineBlock[optionIndex].substr(0, 1) == "*") {
 			score = 100;
@@ -194,13 +238,22 @@ function newTrueFalse(lineBlock) {
 	trueFalse.NewQuestion = 'TF';
 	trueFalse.Title = '';
 	trueFalse.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
-	trueFalse.Points = config.questionScore;
+	var startIndex = 1;
+	if(lineBlock[1].toLowerCase().search("points") != -1) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		trueFalse.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		trueFalse.Points = config.questionScore;
+	}
 	trueFalse.Difficulty = config.difficultyScore;
 	trueFalse.TRUE = 0;
 	trueFalse.FALSE = 0;
-	if (lineBlock[1].toLowerCase() === "true") {
+	if (lineBlock[startIndex].toLowerCase() === "true") {
 		trueFalse.TRUE = 100;
-	} else if (lineBlock[1].toLowerCase() === "false") {
+	} else if (lineBlock[startIndex].toLowerCase() === "false") {
 		trueFalse.FALSE = 100;
 	} else {
 		//TODO Error Reporting
@@ -222,7 +275,169 @@ function newTrueFalse(lineBlock) {
 	return trueFalse;
 }
 
+function newMatching(lineBlock) {
+	var matching = new Object();
+	//Set default settings
+	matching.NewQuestion = 'M';
+	matching.Title = '';
+	matching.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
+	var startIndex = 1;
+	if(lineBlock[1].toLowerCase().search("points") != -1) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		matching.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		matching.Points = config.questionScore;
+	}
+	matching.Difficulty = config.difficultyScore;
+	matching.Options = [];
+	/* Starts at one because the first line of lineBlock is the question*/
+	for (var optionIndex = startIndex; optionIndex < lineBlock.length; optionIndex++) {
+		var answer = lineBlock[optionIndex].split('/');
+		matching.Options.push(["Choice",'"' + optionIndex + '"', '"' + htmlEntities(answer[0]) + '"']);
+		matching.Options.push(["Match",'"' + optionIndex + '"', '"' + htmlEntities(answer[1]) + '"']);
+	}
+	matching.ConvertToCSV = function () {
+		var result, keys;
+		keys = Object.keys(this);
+		result = '';
+		//- 1 magic number is because the ConvertToCSV() counts as a key, and we don't want this function to appear in the CSV
+		for (var i = 0; i < keys.length - 1; i++) {
+			if (keys[i] == "Options") {
+				for (var j = 0; this[keys[i]].length > j; j++) {
+					result += this[keys[i]][j].join();
+					result += '\n';
+				}
+			} else {
+				result += keys[i] + ',' + this[keys[i]];
+				result += '\n';
+			}
+		}
+		return result;
+	}
+	return matching;
+}
 
+function newOrder(lineBlock) {
+	var order = new Object();
+	//Set default settings
+	order.NewQuestion = 'O';
+	order.Title = '';
+	order.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
+	var startIndex = 1;
+	if(lineBlock[1].toLowerCase().search("points") != -1) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		order.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		order.Points = config.questionScore;
+	}
+	order.Difficulty = config.difficultyScore;
+	order.Options = [];
+	/* Starts at one because the first line of lineBlock is the question*/
+	for (var optionIndex = startIndex; optionIndex < lineBlock.length; optionIndex++) {
+		order.Options.push(["Item",'"' + htmlEntities(lineBlock[optionIndex]) + '"']);
+	}
+	order.ConvertToCSV = function () {
+		var result, keys;
+		keys = Object.keys(this);
+		result = '';
+		//- 1 magic number is because the ConvertToCSV() counts as a key, and we don't want this function to appear in the CSV
+		for (var i = 0; i < keys.length - 1; i++) {
+			if (keys[i] == "Options") {
+				for (var j = 0; this[keys[i]].length > j; j++) {
+					result += this[keys[i]][j].join();
+					result += '\n';
+				}
+			} else {
+				result += keys[i] + ',' + this[keys[i]];
+				result += '\n';
+			}
+		}
+		return result;
+	}
+	return order;
+}
+
+function newShortAnswer(lineBlock) {
+	var shortanswer = new Object();
+	//Set default settings
+	shortanswer.NewQuestion = 'SA';
+	shortanswer.Title = '';
+	shortanswer.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
+	var startIndex = 1;
+	if(lineBlock[1].toLowerCase().search("points") != -1) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		shortanswer.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		shortanswer.Points = config.questionScore;
+	}
+	shortanswer.Difficulty = config.difficultyScore;
+	shortanswer.Options = [];
+	/* Starts at one because the first line of lineBlock is the question*/
+	for (var optionIndex = startIndex; optionIndex < lineBlock.length; optionIndex++) {
+		shortanswer.Options.push(['Answer' ,'100','"' + htmlEntities(lineBlock[optionIndex]) + '"']);
+	}
+	shortanswer.ConvertToCSV = function () {
+		var result, keys;
+		keys = Object.keys(this);
+		result = '';
+		//- 1 magic number is because the ConvertToCSV() counts as a key, and we don't want this function to appear in the CSV
+		for (var i = 0; i < keys.length - 1; i++) {
+			if (keys[i] == "Options") {
+				for (var j = 0; this[keys[i]].length > j; j++) {
+					result += this[keys[i]][j].join();
+					result += '\n';
+				}
+			} else {
+				result += keys[i] + ',' + this[keys[i]];
+				result += '\n';
+			}
+		}
+		return result;
+	}
+	return shortanswer;
+}
+
+function newWrittenResponse(lineBlock) {
+	var writtenresponse = new Object();
+	//Set default settings
+	writtenresponse.NewQuestion = 'WR';
+	writtenresponse.Title = '';
+	writtenresponse.QuestionText = '"' + htmlEntities(lineBlock[0]) + '"';
+	var startIndex = 1;
+	if(lineBlock.length === 2) {
+		//Point value given
+		var val = lineBlock[1].split(' ')
+		writtenresponse.Points = htmlEntities(val[1]);
+		startIndex = 2;
+	} else {
+		//Use default
+		writtenresponse.Points = config.questionScore;
+	}
+	writtenresponse.Difficulty = config.difficultyScore;
+	writtenresponse.ConvertToCSV = function () {
+		var result, keys;
+		keys = Object.keys(this);
+		result = '';
+		//- 1 magic number is because the ConvertToCSV() counts as a key, and we don't want this function to appear in the CSV
+		for (var i = 0; i < keys.length - 1; i++) {
+			
+			result += keys[i] + ',' + this[keys[i]];
+			result += '\n';
+			
+		}
+		return result;
+	}
+	return writtenresponse;
+}
 
 //Searches through array of strings and removes empty lines if there's more than one of them in a row. 
 function formatPlainText(array) {
